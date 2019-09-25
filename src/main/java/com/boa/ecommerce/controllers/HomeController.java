deb80f9f58374660a03bbec60e361d0d82afa361
eswaribala <Parameswaribala@gmail.com>
2019-09-25 23:03:38 +0530

    Add files via upload

---
+++ src/main/java/com/boa/ecommerce/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.boa.ecommerce.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/home")
+	public String test()
+	{
+		return "Ready with Ecommerce Application";
+	}
+	
+
+}
